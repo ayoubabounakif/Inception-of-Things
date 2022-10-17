e9e927678465a6a635f220382d96e8f3df3c7734
Ayoub ABOUNAKIF <ayoubabounakif@gmail.com>
2022-10-17 12:55:13 +0100

    [IoT][P3][WIP] - Upgrading script -- Installing K3d + Fix ~/.local/bin path

--- p3/scripts/script.sh
+++ p3/scripts/script.sh
@@ -5,7 +5,6 @@ sudo yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 echo "Docker installed ✅"
 
 ## Post-installation steps for Linux
@@ -16,6 +15,15 @@ sudo usermod -aG docker $USER
 newgrp docker
 sudo systemctl start docker
 echo "Docker changes activated -- Starting Docker ✅"
+
+
+### Installing K3d
+echo "------ Installing current latest release of K3D ⏳ ------"
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+echo "K3D Installed ✅"
+echo "------ Creating a cluster with just a single server node ⏳ ------"
+k3d cluster create mycluster
+echo "K3d cluster created ✅"
 
 
 
@@ -31,14 +39,14 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 ## If you do not have root access on the target system, you can still install kubectl to the ~/.local/bin directory:
 echo "------ Installing kubectl to the ~/.local/bin directory ⏳ ------"
 chmod +x kubectl
-mkdir -p ~/.local/bin
-mv ./kubectl ~/.local/bin/kubectl
+mv ./kubectl /usr/local/bin/kubectl
 echo "Kubectl installed ✅"
-
 
 
 ### Install ArgoCD
 echo "------ Creating ArgoCD Namespace + Applying ArgoCD YAML ⏳ ------"
 kubectl create namespace argocd
+unset http_proxy
+unset https_proxy
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 echo "ArgoCD Installed ✅"
