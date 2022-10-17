fe188da814cc36790bafdf96230f2786d6388f0e
Ayoub ABOUNAKIF <ayoubabounakif@gmail.com>
2022-10-17 16:12:53 +0100

    [IoT][P3][WIP] - ArgoCD Working manually {Needs automation} - Update README.md + Configure port forwarding on Vagrantfile

--- README.md
+++ README.md
@@ -31,4 +31,5 @@ This project aims to introduce you to kubernetes from a developer perspective.Yo
 - kubectl get pod -n argocd
 - kubectl get svc -n argocd
 - kubectl port-forward -n argocd svc/argocd-server 8080:443
-
+- kubectl port-forward --address 0.0.0.0 svc/argocd-server -n argocd 8080:443
+- kubectl wait -n argocd --timeout=180s --for=condition=ready pod -l app.kubernetes.io/name=argocd-server
--- p3/scripts/script.sh
+++ p3/scripts/script.sh
@@ -5,26 +5,16 @@ sudo yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo systemctl enable docker.service
 echo "Docker installed ✅"
 
 ## Post-installation steps for Linux
 ## Managing docker as non-root user
-echo "------ Creating the docker group / Add user to the docker group ⏳ ------"
-sudo groupadd docker
+echo "------ Add user to the docker group ⏳ ------"
 sudo usermod -aG docker $USER
 newgrp docker
-sudo systemctl start docker
-echo "Docker changes activated -- Starting Docker ✅"
-
-
-### Installing K3d
-echo "------ Installing current latest release of K3D ⏳ ------"
-curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-echo "K3D Installed ✅"
-echo "------ Creating a cluster with just a single server node ⏳ ------"
-/usr/local/bin/k3d cluster create mycluster
-echo "K3d cluster created ✅"
-
+sudo systemctl start docker.service
+echo "Docker changes activated -- Starting Docker Service ✅"
 
 
 ### Install kubectl
@@ -35,19 +25,42 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 # curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 # echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 ## 3. Install kubectl
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 ## If you do not have root access on the target system, you can still install kubectl to the ~/.local/bin directory:
-echo "------ Installing kubectl to the ~/.local/bin directory ⏳ ------"
-chmod +x kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl -f
+sudo install -o root -g root -m 0755 ./kubectl /usr/local/bin/kubectl
+export PATH=$PATH:/usr/local/bin
 echo "Kubectl installed ✅"
+
+# echo "------ Install kubectl using native package management [Red Hat-based distributions] ⏳ ------"
+# cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+# [kubernetes]
+# name=Kubernetes
+# baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+# enabled=1
+# gpgcheck=1
+# gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+# EOF
+# sudo yum install -y kubectl
+# echo "Kubectl installed ✅"
+
+### Installing K3d
+echo "------ Installing current latest release of K3d ⏳ ------"
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+echo "K3d Installed ✅"
+
+
+### Creating a k3d cluster
+echo "------ Creating a cluster with just a single server node ⏳ ------"
+k3d cluster create mycluster
+echo "K3d cluster created ✅"
 
 
 ### Install ArgoCD
 echo "------ Creating ArgoCD Namespace + Applying ArgoCD YAML ⏳ ------"
-/usr/local/bin/kubectl create namespace argocd
-unset http_proxy
-unset https_proxy
-/usr/local/bin/kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-/usr/local/bin/kubectl port-forward -n argocd svc/argocd-server 8080:443
+kubectl create namespace argocd
+# unset http_proxy
+# unset https_proxy
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 echo "ArgoCD Installed ✅"
+
+echo "===== Port Forwarding ====="
+# kubectl port-forward -n argocd svc/argocd-server 8080:443
